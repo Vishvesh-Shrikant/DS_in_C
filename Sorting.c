34f62611aa3fe4f9b6027e07144ee60c2d2fe7a0
Vishvesh Shrikant <167464040+Vishvesh-Shrikant@users.noreply.github.com>
2024-11-25 23:40:44 +0530

    quick sort added

--- Sorting.c
+++ Sorting.c
@@ -42,6 +42,43 @@ void selectionSort()
         swap(i, small);
     }
 }
+void quick_sort(int low, int high)
+{
+    int partition_index, pivot, i, j, temp;
+
+    if (low < high)
+    {
+        // Choosing the last element as pivot
+        pivot = array[high];
+        i = low - 1;
+
+        for (j = low; j < high; j++)
+        {
+            if (array[j] <= pivot)
+            {
+                i++;
+                // Swap array[i] and array[j]
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        // Swap pivot element with element at i+1
+        temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        partition_index = i + 1; // that is the index for pivot
+        //to left of pivot , everything is lesser , and to right everything is greater
+
+        // Recursively sort elements before and after partition
+        quick_sort(low, partition_index - 1);
+        quick_sort(partition_index + 1, high);
+    }
+}
+
+
 
 int main()
 {
