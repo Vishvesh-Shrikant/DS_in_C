e3d8a43667de2852379497360622fcd258a947a9
Vishvesh-Shrikant <vishveshshri@gmail.com>
2024-12-05 12:27:13 +0530

    Fibonacci search

--- Searching.c
+++ Searching.c
@@ -4,6 +4,7 @@
 
 int arr[max], n=0;
 
+//LINEAR SEARCH 
 void linearSearch(int val)
 {
     int i;
@@ -17,6 +18,8 @@ void linearSearch(int val)
     }
     printf("Required value %d not found in array.", val);
 }
+
+//BINARY SEARCH 
 void binarySearch(int val)
 {
     int beg=0, end=n-1, mid;
@@ -41,6 +44,48 @@ void binarySearch(int val)
 }
 
 
+// FIBONACCI SEARCH 
+void  fibonacciSearch(int fib1, int fib2, int fibM,  int target) {
+    int offset = -1;
+    while (fibM > 1) {
+        int i = (offset + fib1 < n) ? (offset + fib1) : n - 1;
+
+        if (arr[i] < target) {
+            fibM = fib2;
+            fib2 = fib1;
+            fib1 = fibM - fib2;
+            offset = i;
+        } 
+        else if (arr[i] > target) {
+            fibM = fib1;
+            fib2 = fib2 - fib1;
+            fib1 = fibM - fib2;
+        } 
+        else {
+            printf("Value %d is found at index %d", target, i);
+            return;  // Found
+        }
+    }
+
+    // Check the last element
+    if (fib2 && arr[offset + 1] == target) {
+        printf("Value %d is found at index %d", target, offset+1);
+        return;
+    }
+    printf("Value %d is not found in the array", target);
+    // Not found
+}
+void fibonacci( int val) {
+    int fib1 = 0, fib2 = 1, fibM = fib1 + fib2;
+    while (fibM < n) {
+        fib1 = fib2;
+        fib2 = fibM;
+        fibM = fib1 + fib2;
+    }
+    fibonacciSearch(fib1, fib2, fibM, val );
+}
+
+
 int main()
 {
     int i, val;
@@ -53,7 +98,8 @@ int main()
     }
     printf("\nEnter value to be found:");
     scanf("%d", &val);
-    linearSearch(val);
-    binarySearch(val);
+    //linearSearch(val);
+    //binarySearch(val);
+    //fibonacci(val);
     return 0;
 }
